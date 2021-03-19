a2b0b0e15afe220c30a6afa504a774fb78e23896
Shameer Kunjumohamed <sameerean@gmail.com>
2021-03-19 19:23:19 +0400

    Initial Version

---
+++ src/test/java/hub/poc/quarkus/reactivegraphqlcrud/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package hub.poc.quarkus.reactivegraphqlcrud;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
